ae3ae51420a924e9914874346f779f26e15a3a20
SalmaJr <s.jerouane@gmail.com>
2024-12-21 15:42:20 +0100

    update test1 add file uploader 3

--- test1.py
+++ test1.py
@@ -3,13 +3,13 @@ st.title("This is the app title")
 st.header("This is the header")
 st.markdown("This is the markdown")
 st.subheader("This is the subheader")
-st.caption("This is the caption")
+
 st.sidebar.title("EMC2")
 st.sidebar.image('https://jamiati.ma/sites/default/files/styles/jamiati_card_logo_mobile_2x/public/jamiati-organization/ehtp-1.png?timestamp=1573738706')
 app_type = st.sidebar.selectbox('Select APP Type', ['--- Select a type ---', 'Image analysis', 'OCR', 'Thumbnail imane', 'Face analysis'])
 
 match app_type:
     case 'Image analysis':
-         image_file = st.file_uploader('upload a photo')
+        image_file = st.file_uploader('upload a photo')
     case _:
-        print("pls choose")
+        st.caption("please choose an app type")
