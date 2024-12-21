5fb915b14046933f86c126d2b826ad23ff3ed481
SalmaJr <s.jerouane@gmail.com>
2024-12-21 15:43:41 +0100

    update test1 8

--- test1.py
+++ test1.py
@@ -1,9 +1,6 @@
 import streamlit as st
-st.title("This is the app title")
-st.header("This is the header")
-st.markdown("This is the markdown")
-st.subheader("This is the subheader")
-
+st.title("Module 7 - AI")
+st.header("StreamLit Test")
 st.sidebar.title("EMC2")
 st.sidebar.image('https://jamiati.ma/sites/default/files/styles/jamiati_card_logo_mobile_2x/public/jamiati-organization/ehtp-1.png?timestamp=1573738706')
 app_type = st.sidebar.selectbox('Select APP Type', ['--- Select a type ---', 'Image analysis', 'OCR', 'Thumbnail imane', 'Face analysis'])
